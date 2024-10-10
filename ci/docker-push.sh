cea3016363ece2f17da39a174f95fb7bb027a148
Nikolai Rodionov <allanger@badhouseplants.net>
2024-10-10 17:13:25 +0200

    Only push to onpier oss

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" ghcr.io
-docker buildx build --pull --push -t "$GITHUB_REPOSITORY" -t "$GITHUB_REPOSITORY:$1" -t "ghcr.io/onpier-oss/selenoid:$1" --platform linux/amd64,linux/arm64 .
+docker buildx build --pull --push -t "ghcr.io/onpier-oss/selenoid:$1" --platform linux/amd64,linux/arm64 .
